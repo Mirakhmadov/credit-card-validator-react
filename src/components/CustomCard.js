0ce304183d355e98d4e2c8d7637fd89642e63be0
Warrior_1410 <1410warrior@gmail.com>
2022-10-11 15:38:38 +0500

    adding some css

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -32,129 +32,129 @@ const CustomCard = () => {
     ]
 
     return (
-            <div className="container">
-                <div className="box justify-content-center align-items-center">
-                    <div className="formDiv">
-                        <div className="creditCard">
-                            <Cards
-                                cvc={values.cardSecurityCode}
-                                expiry={values.cardExpirationMonth + values.cardExpirationYear}
-                                focused={values.focus}
-                                name={values.cardName}
-                                number={values.cardNumber}
-                            />
-                        </div>
-                        <Form onSubmit={handleSubmit}>
-                            Card Number
-                            <FormGroup>
-                                <Input
-                                    type="number"
-                                    id="cardNumber"
-                                    data-testid="cardNumber"
-                                    name="cardNumber"
-                                    value={values.cardNumber}
-                                    onChange={handleChange}
-                                    onFocus={handleFocus}
-                                    isValid={errors.cnumber}
-                                    maxLength={16}
-                                />
-                            </FormGroup>
-
-                            <FormGroup>
-                                Card Name
-                                <Input
-                                    type="text"
-                                    id="cardName"
-                                    data-testid="cardName"
-                                    name="cardName"
-                                    value={values.cardName}
-                                    onChange={handleChange}
-                                    onFocus={handleFocus}
-                                    isValid={errors.cname}
-                                    maxLength={50}
-                                />
-                            </FormGroup>
-                            <Row>
-                                <Col>
-                                    Expiration Date
-                                    <Row>
-                                        <Col>
-                                            <FormGroup>
-                                                <Input
-                                                    type="select"
-                                                    id="cardExpirationMonth"
-                                                    data-testid="cardExpirationMonth"
-                                                    name="cardExpirationMonth"
-                                                    value={values.cardExpirationMonth}
-                                                    onChange={handleChange}
-                                                    onFocus={handleFocus}
-                                                    isValid={errors.cexpM}
-                                                >
-                                                    <option selected={true} value={0}>Month</option>
-                                                    {months ? months.map(item =>
-                                                        <option value={item.value}>{item.label}</option>
-                                                    ) : ""}
-                                                </Input>
-                                            </FormGroup>
-                                        </Col>
-                                        <Col>
-                                            <FormGroup>
-                                                <Input
-                                                    type="select"
-                                                    id="cardExpirationYear"
-                                                    data-testid="cardExpirationYear"
-                                                    name="cardExpirationYear"
-                                                    value={values.cardExpirationYear}
-                                                    onChange={handleChange}
-                                                    onFocus={handleFocus}
-                                                    isValid={errors.cexpY}
-                                                >
-                                                    <option selected={true} value={0}>Year</option>
-                                                    {years ? years.map(item =>
-                                                        <option value={item.value}>{item.label}</option>
-                                                    ) : ""}
-                                                </Input>
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                                <Col>
-                                    CVV
-                                    <FormGroup>
-                                        <Input
-                                            type="number"
-                                            id="cardSecurityCode"
-                                            data-testid="cardSecurityCode"
-                                            name="cardSecurityCode"
-                                            value={values.cardSecurityCode}
-                                            onChange={handleChange}
-                                            onFocus={handleFocus}
-                                            isValid={errors.ccvv}
-                                            maxLength={3}
-                                        />
-                                    </FormGroup>
-                                </Col>
-                            </Row>
-                            <Button
-                                size={"block"}
-                                data-testid="validateButton"
-                                id="validateButton"
-                                type="submit"
-                            >
-                                Validate
-                            </Button>
-                        </Form>
+        <div className="container">
+            <div className="box justify-content-center align-items-center">
+                <div className="formDiv">
+                    <div className="creditCard">
+                        <Cards
+                            cvc={values.cardSecurityCode}
+                            expiry={values.cardExpirationMonth + values.cardExpirationYear}
+                            focused={values.focus}
+                            name={values.cardName}
+                            number={values.cardNumber}
+                        />
                     </div>
-                    <Alert
-                        id="alertMessage"
-                        data-testid="alertMessage"
-                        color={errors.variant}
-                        isOpen={errors.show}
-                    >
-                        {errors.message}
-                    </Alert>{" "}
+                    <Form onSubmit={handleSubmit}>
+                        Card Number
+                        <FormGroup>
+                            <Input
+                                type="number"
+                                id="cardNumber"
+                                data-testid="cardNumber"
+                                name="cardNumber"
+                                value={values.cardNumber}
+                                onChange={handleChange}
+                                onFocus={handleFocus}
+                                isValid={errors.cnumber}
+                                maxLength={16}
+                            />
+                        </FormGroup>
+
+                        <FormGroup>
+                            Card Name
+                            <Input
+                                type="text"
+                                id="cardName"
+                                data-testid="cardName"
+                                name="cardName"
+                                value={values.cardName}
+                                onChange={handleChange}
+                                onFocus={handleFocus}
+                                isValid={errors.cname}
+                                maxLength={50}
+                            />
+                        </FormGroup>
+                        <Row>
+                            <Col>
+                                Expiration Date
+                                <Row>
+                                    <Col>
+                                        <FormGroup>
+                                            <Input
+                                                type="select"
+                                                id="cardExpirationMonth"
+                                                data-testid="cardExpirationMonth"
+                                                name="cardExpirationMonth"
+                                                value={values.cardExpirationMonth}
+                                                onChange={handleChange}
+                                                onFocus={handleFocus}
+                                                isValid={errors.cexpM}
+                                            >
+                                                <option selected={true} value={0}>Month</option>
+                                                {months ? months.map(item =>
+                                                    <option value={item.value}>{item.label}</option>
+                                                ) : ""}
+                                            </Input>
+                                        </FormGroup>
+                                    </Col>
+                                    <Col>
+                                        <FormGroup>
+                                            <Input
+                                                type="select"
+                                                id="cardExpirationYear"
+                                                data-testid="cardExpirationYear"
+                                                name="cardExpirationYear"
+                                                value={values.cardExpirationYear}
+                                                onChange={handleChange}
+                                                onFocus={handleFocus}
+                                                isValid={errors.cexpY}
+                                            >
+                                                <option selected={true} value={0}>Year</option>
+                                                {years ? years.map(item =>
+                                                    <option value={item.value}>{item.label}</option>
+                                                ) : ""}
+                                            </Input>
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col>
+                                CVV
+                                <FormGroup>
+                                    <Input
+                                        type="number"
+                                        id="cardSecurityCode"
+                                        data-testid="cardSecurityCode"
+                                        name="cardSecurityCode"
+                                        value={values.cardSecurityCode}
+                                        onChange={handleChange}
+                                        onFocus={handleFocus}
+                                        isValid={errors.ccvv}
+                                        maxLength={3}
+                                    />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Button
+                            size={"block"}
+                            data-testid="validateButton"
+                            id="validateButton"
+                            type="submit"
+                        >
+                            Validate
+                        </Button>
+                    </Form>
                 </div>
+                <Alert
+                    id="alertMessage"
+                    data-testid="alertMessage"
+                    color={errors.variant}
+                    isOpen={errors.show}
+                >
+                    {errors.message}
+                </Alert>{" "}
             </div>
+        </div>
     );
 }
 
